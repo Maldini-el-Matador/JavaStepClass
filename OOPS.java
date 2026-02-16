44a63c5563767d9f9053b0fdfe6fbe77cef1547b
Kr Pranav <pranav14109@gmail.com>
2026-02-16 09:26:11 +0530

    oops banner Use case 2

--- OOPS.java
+++ OOPS.java
@@ -1,5 +1,11 @@
 public class OOPS{
-	public static void main(String args[]){
-		System.out.println("*OOPS*");
-	}
+ public static void main(String args[]){
+    System.out.println("  *** " + "   *** " + " ***** " + "   **** ");
+    System.out.println(" ** ** " + " ** ** " + " ** ** " + "**  **");
+    System.out.println(" ** ** " + " ** ** " + " ** ** " + " **  ");
+    System.out.println(" ** ** " + " ** ** " + " ***** " + "  ** ");
+    System.out.println(" ** ** " + " ** ** " + " ** " + "       ** ");
+    System.out.println(" ** ** " + " ** ** " + " ** " + " **  ** ");
+    System.out.println("  *** " + "   *** " + "  ** " + "  **** ");
+    }
 }
